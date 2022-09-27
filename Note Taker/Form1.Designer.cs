6523bae27b77c7ba74ddb771556e196895aa4439
Ignisnex <pbacarpenter@gmail.com>
2022-09-27 15:13:39 -0600

    Working first revision of the small Note Taker app. Saves notes to a local file in the user Documents folder.

---
+++ Note Taker/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace Note_Taker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.delBtn = new System.Windows.Forms.Button();
+            this.noteList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.noteName = new System.Windows.Forms.TextBox();
+            this.noteBox = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.savBtn = new System.Windows.Forms.Button();
+            this.newBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // delBtn
+            // 
+            this.delBtn.Location = new System.Drawing.Point(365, 37);
+            this.delBtn.Name = "delBtn";
+            this.delBtn.Size = new System.Drawing.Size(133, 23);
+            this.delBtn.TabIndex = 2;
+            this.delBtn.Text = "Delete Selected Note";
+            this.delBtn.UseVisualStyleBackColor = true;
+            this.delBtn.Click += new System.EventHandler(this.delBtn_Click);
+            // 
+            // noteList
+            // 
+            this.noteList.FormattingEnabled = true;
+            this.noteList.ItemHeight = 15;
+            this.noteList.Location = new System.Drawing.Point(12, 37);
+            this.noteList.Name = "noteList";
+            this.noteList.Size = new System.Drawing.Size(337, 154);
+            this.noteList.TabIndex = 3;
+            this.noteList.SelectedIndexChanged += new System.EventHandler(this.noteList_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Note List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 198);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Note Name";
+            // 
+            // noteName
+            // 
+            this.noteName.Location = new System.Drawing.Point(12, 217);
+            this.noteName.Name = "noteName";
+            this.noteName.Size = new System.Drawing.Size(337, 23);
+            this.noteName.TabIndex = 6;
+            // 
+            // noteBox
+            // 
+            this.noteBox.Location = new System.Drawing.Point(12, 277);
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(486, 161);
+            this.noteBox.TabIndex = 7;
+            this.noteBox.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Note";
+            // 
+            // savBtn
+            // 
+            this.savBtn.Location = new System.Drawing.Point(365, 217);
+            this.savBtn.Name = "savBtn";
+            this.savBtn.Size = new System.Drawing.Size(133, 23);
+            this.savBtn.TabIndex = 9;
+            this.savBtn.Text = "Save Changes";
+            this.savBtn.UseVisualStyleBackColor = true;
+            this.savBtn.Click += new System.EventHandler(this.savBtn_Click);
+            // 
+            // newBtn
+            // 
+            this.newBtn.Location = new System.Drawing.Point(365, 188);
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(133, 23);
+            this.newBtn.TabIndex = 10;
+            this.newBtn.Text = "New Note";
+            this.newBtn.UseVisualStyleBackColor = true;
+            this.newBtn.Click += new System.EventHandler(this.newBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 450);
+            this.Controls.Add(this.newBtn);
+            this.Controls.Add(this.savBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.noteBox);
+            this.Controls.Add(this.noteName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.noteList);
+            this.Controls.Add(this.delBtn);
+            this.Name = "Form1";
+            this.Text = "Note Taker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button delBtn;
+        private System.Windows.Forms.ListBox noteList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox noteName;
+        private System.Windows.Forms.RichTextBox noteBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button savBtn;
+        private System.Windows.Forms.Button newBtn;
+    }
+}
+
---
+++ Note Taker/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Note_Taker
+{
+    public partial class Form1 : Form
+
+
+    {
+        static DataTable dt;
+        static readonly string path = System.IO.Path.Combine(Environment.GetFolderPath(
+                          Environment.SpecialFolder.MyDoc‌​uments), "NoteTaker", "SavedNotes.xml");
+        static readonly string dirPath = System.IO.Path.Combine(Environment.GetFolderPath(
+                          Environment.SpecialFolder.MyDoc‌​uments), "NoteTaker");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*
+             * On form load, establish the data items we'll need.
+             * Initialize the DataTable, and create the columns we need.
+             * Check if there is a save file.
+             * Refresh the screen so the user gets an updated look of the saved file.
+             */ 
+            dt = new DataTable();
+            dt.TableName = "Notes";
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("Note", typeof(string));
+            getSaveFile();
+            refresh();
+        }
+
+        static void getSaveFile()
+        {
+            /*
+             * Check if there is a save file.
+             * If there isn't, create the NoteTaker folder, and SavedNotes.xml
+             * If there is, dump the data in our DataTable, and load the file instead
+             */
+            if (!System.IO.File.Exists(path))
+            {
+                System.IO.Directory.CreateDirectory(dirPath);
+                var filePath = System.IO.File.Create(path);
+                filePath.Close();
+                dt.WriteXml(path, XmlWriteMode.WriteSchema);
+            }
+            else
+            {
+                dt.Rows.Clear();
+                dt.ReadXml(path);
+            }
+        }
+
+        private void savBtn_Click(object sender, EventArgs e)
+        {
+            /*
+             * Saves all the notes when the Save Changes button is clicked
+             * Ensures no Out of Bounds errors will happen, right off the bat.
+             * If an index is selected in the Note List, overwrite the data at that
+             * index in the DataTable, otherwise add a new row to the DataTable.
+             * Save entire DataTable to SavedNotes.xml, and refresh the Notes List.
+             */
+            int index = noteList.SelectedIndex;
+            if (index > -1)
+            {
+                dt.Rows[index]["Name"] = noteName.Text;
+                dt.Rows[index]["Note"] = noteBox.Text;
+            }
+            else
+            {
+                dt.Rows.Add(noteName.Text, noteBox.Text);
+            }
+            dt.WriteXml(path, XmlWriteMode.WriteSchema);
+            refresh();
+
+        }
+
+        public void refresh() 
+        {
+            /*
+             * Clears the DataTable, and pulls the data from SavedNotes.xml,
+             * then clears the Notes List and populates it again with the DataTable.
+             */
+            dt.Rows.Clear();
+            dt.ReadXml(path);
+            noteList.Items.Clear();
+            for(int i = 0; i < dt.Rows.Count; i++)
+            {
+                noteList.Items.Add(dt.Rows[i].ItemArray[0].ToString());
+            }
+        }
+
+        private void noteList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*
+             * Populates the Note Name and Note boxes with the DataTable contents
+             * at a selected index of the Notes List
+             */
+            int index = noteList.SelectedIndex;
+            if (index > -1)
+            {
+                noteName.Text = dt.Rows[index].ItemArray[0].ToString();
+                noteBox.Text = dt.Rows[index].ItemArray[1].ToString();
+            }
+        }
+
+        private void delBtn_Click(object sender, EventArgs e)
+        {
+            /*
+             * Removes the DataTable row at the index selected in the Notes List.
+             * Ensures no Out of Bounds errors, then removes at the specified index.
+             * Writes updated table to SavedNotes.xml, then refreshes Notes List.
+             */
+            int index = noteList.SelectedIndex;
+            if (index > -1)
+            {
+                dt.Rows[index].Delete();
+                dt.WriteXml(path, XmlWriteMode.WriteSchema);
+            }
+            refresh();
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            /*
+             * Clears selected index from Notes List so saved note will create new row.
+             */
+            noteList.ClearSelected();
+            noteBox.Clear();
+            noteName.Clear();
+        }
+    }
+}
